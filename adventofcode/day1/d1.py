afb15b8f8a4bf1b70f94c3f6e5553d687c71f5c1
Niko Böckerman <git.585wj@slmail.me>
2023-12-01 21:47:35 +0000

    Day 1 solution

---
+++ adventofcode/day1/d1.py
@@ -0,0 +1,51 @@
+import logging
+import pathlib
+from typing import Iterable
+
+STR_DIGITS = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+
+def find_first_and_last_int(input: str) -> tuple[str, str]:
+    first: str | None = None
+    last: str | None = None
+    for ind, c in enumerate(input):
+        val = None
+        if c.isdigit():
+            val = c
+        else:
+            for digit_ind, str_digit in enumerate(STR_DIGITS):
+                if input.find(str_digit, ind, ind + len(str_digit)) >= 0:
+                    val = str(digit_ind + 1)
+                    break
+
+        if val is not None:
+            if first is None:
+                first = val
+                last = val
+            else:
+                last = val
+
+    assert first is not None and last is not None
+    return first, last
+
+
+def int_chars_to_int(s1: str, s2: str) -> int:
+    return int(s1 + s2)
+
+
+def p1_ints(lines: Iterable[str]) -> Iterable[int]:
+    for line in lines:
+        logging.debug(f"{line=}")
+        value = int_chars_to_int(*find_first_and_last_int(line))
+        logging.debug(f"{value=}")
+        yield value
+
+
+def d1() -> None:
+    logging.basicConfig(level=logging.DEBUG)
+    lines = (pathlib.Path(__file__).parent / "input1-raw.txt").read_text().splitlines()
+    print(sum(p1_ints(lines)))
+
+
+if __name__ == "__main__":
+    d1()
