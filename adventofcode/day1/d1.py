bdb0d74de31559f99d26b9e5a66cf79a110b4370
Niko Böckerman <git.585wj@slmail.me>
2023-12-02 19:34:38 +0000

    Separate solvers for day1 problems

--- adventofcode/day1/d1.py
+++ adventofcode/day1/d1.py
@@ -5,14 +5,14 @@ from typing import Iterable
 STR_DIGITS = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 
-def find_first_and_last_int(input: str) -> tuple[str, str]:
+def find_first_and_last_int(input: str, use_texts: bool) -> tuple[str, str]:
     first: str | None = None
     last: str | None = None
     for ind, c in enumerate(input):
         val = None
         if c.isdigit():
             val = c
-        else:
+        elif use_texts:
             for digit_ind, str_digit in enumerate(STR_DIGITS):
                 if input.find(str_digit, ind, ind + len(str_digit)) >= 0:
                     val = str(digit_ind + 1)
@@ -33,19 +33,19 @@ def int_chars_to_int(s1: str, s2: str) -> int:
     return int(s1 + s2)
 
 
-def p1_ints(lines: Iterable[str]) -> Iterable[int]:
+def p1_ints(lines: Iterable[str], use_texts: bool) -> Iterable[int]:
     for line in lines:
         logging.debug(f"{line=}")
-        value = int_chars_to_int(*find_first_and_last_int(line))
+        value = int_chars_to_int(*find_first_and_last_int(line, use_texts))
         logging.debug(f"{value=}")
         yield value
 
 
-def d1() -> None:
-    logging.basicConfig(level=logging.DEBUG)
+def p1() -> None:
     lines = (pathlib.Path(__file__).parent / "input1-raw.txt").read_text().splitlines()
-    print(sum(p1_ints(lines)))
+    print(sum(p1_ints(lines, False)))
 
 
-if __name__ == "__main__":
-    d1()
+def p2() -> None:
+    lines = (pathlib.Path(__file__).parent / "input1-raw.txt").read_text().splitlines()
+    print(sum(p1_ints(lines, True)))
