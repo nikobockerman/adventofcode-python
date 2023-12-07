a5f047cc8a738f1e010b69e5b7cb74a0dce3cb1c
Niko Böckerman <git.585wj@slmail.me>
2023-12-07 19:20:25 +0000

    Day 7 solutions

---
+++ adventofcode/d7.py
@@ -0,0 +1,150 @@
+from dataclasses import dataclass
+import enum
+from functools import total_ordering
+from types import NotImplementedType
+from typing import Iterable
+
+
+def _parse_input(lines: Iterable[str]) -> Iterable[tuple[str, int]]:
+    for line in lines:
+        cards, bid = line.split()
+        yield cards, int(bid)
+
+
+@total_ordering
+class HandType(enum.Enum):
+    HighCard = 0
+    OnePair = 1
+    TwoPair = 2
+    ThreeOfAKind = 3
+    FullHouse = 4
+    FourOfAKind = 5
+    FiveOfAKind = 6
+
+    def __lt__(self, other: "HandType") -> bool | NotImplementedType:
+        if self.__class__ is other.__class__:
+            return self.value < other.value
+        return NotImplemented
+
+
+@dataclass
+class Hand:
+    card_values: list[int]
+    hand_type: HandType
+    bid: int
+
+    def __lt__(self, other: "Hand") -> bool:
+        if self.hand_type != other.hand_type:
+            return self.hand_type < other.hand_type
+
+        for i in range(5):
+            if self.card_values[i] != other.card_values[i]:
+                return self.card_values[i] < other.card_values[i]
+        assert False
+
+
+def p1(input: str) -> int:
+    d = _parse_input(input.splitlines())
+
+    def classify_hand_type(cards: str) -> HandType:
+        assert len(cards) == 5
+        value_counts: dict[str, int] = {}
+        for c in cards:
+            value_counts[c] = value_counts.get(c, 0) + 1
+
+        counts = sorted(value_counts.items(), key=lambda x: x[1], reverse=True)
+        if counts[0][1] == 5:
+            return HandType.FiveOfAKind
+        elif counts[0][1] == 4:
+            return HandType.FourOfAKind
+        elif counts[0][1] == 3:
+            if counts[1][1] == 2:
+                return HandType.FullHouse
+            else:
+                return HandType.ThreeOfAKind
+        elif counts[0][1] == 2:
+            if counts[1][1] == 2:
+                return HandType.TwoPair
+            else:
+                return HandType.OnePair
+        else:
+            return HandType.HighCard
+
+    def card_value(value: str) -> int:
+        assert len(value) == 1
+        try:
+            return int(value)
+        except ValueError:
+            pass
+
+        return "TJQKA".index(value) + 10
+
+    hands = [
+        Hand(
+            [card_value(c) for c in cards],
+            classify_hand_type(cards),
+            bid,
+        )
+        for cards, bid in d
+    ]
+
+    hands.sort()
+
+    return sum((ind + 1) * hand.bid for ind, hand in enumerate(hands))
+
+
+def p2(input: str) -> int:
+    d = _parse_input(input.splitlines())
+
+    def classify_hand_type(cards: str) -> HandType:
+        assert len(cards) == 5
+        value_counts: dict[str, int] = {}
+        for c in cards:
+            value_counts[c] = value_counts.get(c, 0) + 1
+
+        jokers = value_counts.get("J", 0)
+        value_counts.pop("J", None)
+
+        if jokers == 5:
+            return HandType.FiveOfAKind
+
+        counts = sorted(value_counts.items(), key=lambda x: x[1], reverse=True)
+        if counts[0][1] + jokers == 5:
+            return HandType.FiveOfAKind
+        elif counts[0][1] + jokers == 4:
+            return HandType.FourOfAKind
+        elif counts[0][1] + jokers == 3:
+            if counts[1][1] == 2:
+                return HandType.FullHouse
+            else:
+                return HandType.ThreeOfAKind
+        elif counts[0][1] + jokers == 2:
+            if counts[1][1] == 2:
+                return HandType.TwoPair
+            else:
+                return HandType.OnePair
+        else:
+            return HandType.HighCard
+
+    def card_value(value: str) -> int:
+        assert len(value) == 1
+        try:
+            return int(value)
+        except ValueError:
+            pass
+        if value == "J":
+            return 1
+        return "TQKA".index(value) + 10
+
+    hands = [
+        Hand(
+            [card_value(c) for c in cards],
+            classify_hand_type(cards),
+            bid,
+        )
+        for cards, bid in d
+    ]
+
+    hands.sort()
+
+    return sum((ind + 1) * hand.bid for ind, hand in enumerate(hands))
