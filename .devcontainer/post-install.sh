01098702a2657eacbe3f2daf19a67f4a9ab2a712
Niko Böckerman <git.585wj@slmail.me>
2023-12-14 21:23:33 +0000

    Improved devcontainer

---
+++ .devcontainer/post-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+WORKSPACE_DIR=$(pwd)
+
+# Change some Poetry settings to make it more friendly in a container
+poetry config cache-dir ${WORKSPACE_DIR}/.poetry_cache
+poetry config virtualenvs.in-project true
+
+# Now install all dependencies, including dev dependencies
+poetry install --with=dev
+
+echo "Done!"
