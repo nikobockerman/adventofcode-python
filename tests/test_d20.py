6f90447569fa6ba0c76d1d8851e51e6a16bdfdee
Niko Böckerman <git.585wj@slmail.me>
2024-08-02 15:29:43 +0000

    Remove useless and invalid Day 20 Part 2 test

--- tests/test_d20.py
+++ tests/test_d20.py
@@ -23,20 +23,3 @@ def test_p1_example1():
 
 def test_p1_example2():
     assert d20.p1(example2_input) == 11687500
-
-
-simple_input = r"""
-broadcaster -> a, b
-%a -> ha
-%b -> hb, c
-%c -> ha
-&ha -> ka, kc
-&hb -> ka, kc
-&ka -> kx
-&kc -> kx
-&kx -> rx
-""".strip()
-
-
-def test_p2_example2():
-    assert d20.p2(simple_input) == 1
