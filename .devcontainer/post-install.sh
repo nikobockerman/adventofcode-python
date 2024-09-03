357d90b6a950261f178916bc8a825a75042c005e
Niko Böckerman <git.585wj@slmail.me>
2024-09-03 19:59:12 +0000

    Add shellcheck to CI and fix issue
    Execute shellcheck only if it is installed. It's considered optional
    dependency due to it not being installable via poetry.

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -5,7 +5,7 @@ set -ex
 WORKSPACE_DIR=$(pwd)
 
 # Change some Poetry settings to make it more friendly in a container
-poetry config cache-dir ${WORKSPACE_DIR}/.poetry_cache
+poetry config cache-dir "${WORKSPACE_DIR}"/.poetry_cache
 poetry config virtualenvs.in-project true
 
 # Now install all dependencies, including dev dependencies
