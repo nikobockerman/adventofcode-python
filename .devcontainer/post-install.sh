d54cb14b77f4af29c49cc06adf51df09bd596813
Niko Böckerman <git.585wj@slmail.me>
2024-09-03 19:48:34 +0000

    devcontainer: post-install to sync poetry deps

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -9,6 +9,6 @@ poetry config cache-dir ${WORKSPACE_DIR}/.poetry_cache
 poetry config virtualenvs.in-project true
 
 # Now install all dependencies, including dev dependencies
-poetry install --with=dev
+poetry install --with=dev --sync
 
 echo "Done!"
