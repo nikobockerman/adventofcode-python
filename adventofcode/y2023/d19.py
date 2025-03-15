52a36e280891cb22672789c7d03442d6dc034949
Niko Böckerman <git.585wj@slmail.me>
2025-03-15 07:06:53 +0000

    Quote types in cast() calls
    TC006 from ruff. Calling cast with quoted types is generally faster
    than without quotes.

--- adventofcode/y2023/d19.py
+++ adventofcode/y2023/d19.py
@@ -332,7 +332,7 @@ def p2(input_str: str) -> int:
             assert all(
                 ranges is not None for ranges in step.category_value_ranges.values()
             )
-            ranges = cast(dict[_Category, list[range]], step.category_value_ranges)
+            ranges = cast("dict[_Category, list[range]]", step.category_value_ranges)
             accepted_category_value_ranges.append(ranges)
             continue
 
